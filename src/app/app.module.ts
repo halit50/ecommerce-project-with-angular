24e97a8bfc8acea4f5b744ab70cfea3ba34a65b9
halitcinici <halit.cinici@ebp.com>
2024-02-06 11:53:03 +0100

    creation du modele de données product et ajout des images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ContainerComponent } from './container/container.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContainerComponent } from './components/container/container.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { CardComponent } from './card/card.component';
     NavbarComponent,
     ContainerComponent,
     FooterComponent,
-    CardComponent
+    CardComponent,
+    ProductComponent,
+    ProductItemComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
